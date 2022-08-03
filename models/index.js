a9130739a3fc2f3f209eb98eec14978e8baee993
charlie <gn4rtistic@gmail.com>
2022-08-02 18:16:38 -0700

    created model for user and definined relationships in index.js

---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import all models
+const Listing = require( './Listing' );
+const User = require( './User' );
+const Category = require( './Category' );
+const Tags = require( './Tags' );
+const ListingTag = require( './Listing-tags' );
+
+// create associations
+User.hasMany( Listing, {
+    foreignKey: 'user_id'
+} );
+
+Listing.belongsTo( User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+} );
+
+Category.hasMany( Listing, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
+} );
+
+Listing.belongsToMany( Tags, {
+    through: ListingTag
+} );
+
+Tags.belongsToMany( Listing, {
+    through: ListingTag
+} );
+
+module.exports = { User, Listing, Category, ListingTag, Tags };
