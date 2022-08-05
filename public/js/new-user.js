4df5a26c7ef2301d73403dd25f4c8eeed2eb477c
charlie <gn4rtistic@gmail.com>
2022-08-05 01:11:25 -0700

    modals on homepage handled

--- public/js/new-user.js
+++ public/js/new-user.js
@@ -1,11 +1,17 @@
 // modal
-const signupBtn = document.querySelector( '#signup' );
+const signUpBtn = document.querySelector( '#signUp' );
 const modalBg = document.querySelector( '.modal-background' );
 const modalClose = document.querySelector('#modalClose')
 const signUpModal = document.querySelector( '.signUpModal' );
 const formSubmit = document.querySelector( '#formSubmit' );
+// listing modal
+const listingBtn = document.querySelector( '.listing' );
+const listingModal = document.querySelector( '.listingModal' );
 
-signupBtn.addEventListener( 'click', () =>
+const modalBgListing = document.querySelector( '#modalBgListing' );
+
+
+signUpBtn.addEventListener( 'click', () =>
 {
     signUpModal.classList.add( 'is-active' );
 } );
@@ -18,4 +24,14 @@ modalBg.addEventListener( 'click', () =>
 formSubmit.addEventListener( 'click', () =>
 {
     
+} );
+//listing modal
+listingBtn.addEventListener( 'click', () =>
+{
+    listingModal.classList.add( 'is-active' );
+} );
+
+modalBgListing.addEventListener( 'click', () =>
+{
+    listingModal.classList.remove( 'is-active' );
 } );
