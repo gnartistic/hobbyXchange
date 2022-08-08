89ccfa837ddf94ea93c5efa22163fb99795912b8
Strider <dave.barnes23@gmail.com>
2022-08-08 14:22:32 -0500

    finished building api folder routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,12 @@ const router = require( 'express' ).Router();
 const userRoutes = require( './user-routes.js' );
 const listingRoutes = require( './listing-routes' );
 const categoryRoutes = require( './category-routes' );
-const tagRoutes = require( './tag-routes' );
+// const tagRoutes = require( './tag-routes' );
 
 
 router.use( '/users', userRoutes );
 router.use( '/listings', listingRoutes );
 router.use( '/categories', categoryRoutes );
-router.use( '/tags', tagRoutes );
+// router.use( '/tags', tagRoutes );
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,161 @@
 const router = require( 'express' ).Router();
 const { User, Listing} = require( '../../models' );
 
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: [
+            {exclude: ['password']},
+            'id',
+            'username',
+            'email',
+            'name',
+            'wish_list'
+        ],
+        include: [
+            {
+                model: Listing,
+                    attributes: ['id', 'title', 'description', 'user_id']
+            }
+        ]
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            {exclude: ['password']},
+            'id',
+            'username',
+            'email',
+            'name',
+            'wish_list'
+        ],
+        include: [
+            {
+                model: Listing,
+                    attributes: ['id', 'title', 'description', 'user_id']
+            }
+        ] 
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'There is no User matching that ID'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        wish_list: req.body.wish_list
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Wrong password' });
+            return;
+        }
+        
+        const passwordConfirm = dbUserData.checkPassword(res.body.password);
+        if(!passwordConfirm) {
+            res.status(400).json({ message: 'Wrong password' });
+            return;
+        }
+
+        res.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'Login Successful!' });
+        });
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(200).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'There is no User matching that ID' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'There is no User matching that ID' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
