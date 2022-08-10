390d082a8bb4d4a80639b42dc9b4ad1c0b9b2ec9
Strider <dave.barnes23@gmail.com>
2022-08-09 20:50:39 -0500

    added color to listed by on mainpage

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const routes = require('./controllers');
 const sequelize = require( './config/connection' );
 const path = require( 'path' );
 
+const helpers = require('./utils/helpers');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require( 'express-handlebars' );
-const hbs = exphbs.create( {} );
+const hbs = exphbs.create( { helpers } );
 const session = require( 'express-session' );
 const SequelizeStore = require( 'connect-session-sequelize' )( session.Store );
 const sess = {
