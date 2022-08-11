33f24fa917a86d78043900300548ed6981a6ad8f
Strider <dave.barnes23@gmail.com>
2022-08-11 17:45:50 -0500

    create new listing now functional

--- controllers/api/listing-routes.js
+++ controllers/api/listing-routes.js
@@ -73,7 +73,7 @@ router.post('/', (req, res) => {
             title: req.body.title,
             description: req.body.description,
             // change back to session instead of body once path confirmed working
-            user_id: req.body.user_id,
+            user_id: req.session.user_id,
             category_id: req.body.category_id
         })
         .then(dbListingData => res.json(dbListingData))
