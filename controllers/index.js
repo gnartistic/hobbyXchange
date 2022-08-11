47cb24f961511c0b0b75dd01a67c5ae20c72a30d
Strider <dave.barnes23@gmail.com>
2022-08-10 21:32:19 -0500

    walked it back to the 7pm commit

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require( 'express' ).Router();
 
 const apiRoutes = require( './api/' );
 const homeRoutes = require( './home-routes.js' );
-const createListingRoutes = require( './dashboard-routes.js' );
+const createListingRoutes = require( './create-listing-route.js' );
 const userListingRoutes = require( './dashboard-routes.js');
 
 router.use( '/', homeRoutes );
