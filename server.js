f18f931cf45152c5e9d8ab25dc35b93867b536ee
Strider <dave.barnes23@gmail.com>
2023-04-19 08:57:19 -0500

    had duplicated a folder that has been removed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sequelize = require( './config/connection' );
 const path = require( 'path' );
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const exphbs = require( 'express-handlebars' );
 const session = require( 'express-session' );
 const SequelizeStore = require( 'connect-session-sequelize' )( session.Store );
